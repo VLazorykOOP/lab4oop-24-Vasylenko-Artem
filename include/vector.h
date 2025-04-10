b950079142d01c0cc41394076dfb81dd443dff39
artemka <artem.vas333@gmail.com>
2025-04-10 11:22:28 +0300

    add VectorInt and operators postfix and prefix (++ and --)

--- include/vector.h
+++ include/vector.h
@@ -5,37 +5,27 @@
 #include "console.h"
 #include "taskManager.h"
 
-class Vector
+class VectorInt
 {
 private:
 	int *data;
 	int size;
-	int state;
-	static int objectCount;
+	int codeError;
 
 public:
-	Vector();
-	Vector(int n);
-	Vector(int n, int value);
-	Vector(const Vector &other);
+	VectorInt();
+	VectorInt(int n);
+	VectorInt(int n, int value);
 
-	~Vector();
+	VectorInt(const VectorInt &other);
 
-	void set(int index, int value = 0);
-	int get(int index) const;
+	~VectorInt();
+
+	VectorInt &operator++();
+	VectorInt operator++(int);
+
+	VectorInt &operator--();
+	VectorInt operator--(int);
+
 	void print() const;
-
-	Vector &operator=(const Vector &other);
-
-	Vector operator+(const Vector &other) const;
-	Vector operator-(const Vector &other) const;
-	Vector operator*(short scalar) const;
-
-	bool operator==(const Vector &other) const;
-	bool operator!=(const Vector &other) const;
-	bool operator<(const Vector &other) const;
-	bool operator>(const Vector &other) const;
-
-	int getState() const;
-	static int getObjectCount();
 };
--- src/class/vector.cpp
+++ src/class/vector.cpp
@@ -2,143 +2,87 @@
 
 using namespace std;
 
-Vector::Vector() : size(1), state(0)
-{
-	data = new (nothrow) int[1];
-	if (!data)
-		state = 1;
-	else
-		data[0] = 0;
-	objectCount++;
-}
-
-Vector::Vector(int n) : size(n), state(0)
-{
-	data = new (nothrow) int[n];
-	if (!data)
-		state = 1;
-	else
-		fill(data, data + n, 0);
-	objectCount++;
-}
-
-Vector::Vector(int n, int value) : size(n), state(0)
-{
-	data = new (nothrow) int[n];
-	if (!data)
-		state = 1;
-	else
-		fill(data, data + n, value);
-	objectCount++;
-}
-
-Vector::Vector(const Vector &other) : size(other.size), state(other.state)
+VectorInt::VectorInt() : size(1), codeError(0)
 {
 	data = new (nothrow) int[size];
 	if (!data)
-		state = 1;
-	else
-		copy(other.data, other.data + size, data);
-	objectCount++;
-}
-
-Vector &Vector::operator=(const Vector &other)
-{
-	if (this != &other)
 	{
-		delete[] data;
-		size = other.size;
-		state = other.state;
-		data = new (nothrow) int[size];
-		if (!data)
-			state = 1;
-		else
-			copy(other.data, other.data + size, data);
+		codeError = 2;
+		return;
 	}
-	return *this;
 }
 
-Vector::~Vector()
+VectorInt::VectorInt(int n) : size(n), codeError(0)
 {
-	cout << "Vector destructor called" << endl;
-	delete[] data;
-	objectCount--;
-}
-
-void Vector::set(int index, int value)
-{
-	if (index >= size)
+	data = new (nothrow) int[size];
+	if (!data)
 	{
-		state = 1;
-		throw out_of_range("Index out of bounds");
+		codeError = 2;
+		return;
 	}
-	data[index] = value;
+
+	for (int i = 0; i < n; i++)
+		data[i] = 0;
 }
 
-int Vector::get(int index) const
+VectorInt::VectorInt(int n, int value) : size(n), codeError(0)
 {
-	if (index >= size)
-		throw out_of_range("Index out of bounds");
+	data = new (nothrow) int[size];
+	if (!data)
+	{
+		codeError = 2;
+		return;
+	}
 
-	return data[index];
+	for (int i = 0; i < n; i++)
+		data[i] = value;
 }
 
-void Vector::print() const
+VectorInt::VectorInt(const VectorInt &other) : size(other.size), codeError(0)
 {
+	data = new (nothrow) int[size];
+	if (!data)
+	{
+		codeError = 2;
+		return;
+	}
 	for (int i = 0; i < size; i++)
+		data[i] = other.data[i];
+}
+
+VectorInt::~VectorInt() { delete[] data; }
+
+void VectorInt::print() const
+{
+	for (int i = 0; i < size; ++i)
 		cout << data[i] << " ";
 	cout << endl;
 }
 
-Vector Vector::operator+(const Vector &other) const
+VectorInt &VectorInt::operator++()
 {
-	if (size != other.size)
-		throw invalid_argument("Vectors must be of the same size");
-
-	Vector result(size);
 	for (int i = 0; i < size; i++)
-		result.data[i] = data[i] + other.data[i];
-
-	return result;
+		++data[i];
+	return *this;
 }
 
-Vector Vector::operator-(const Vector &other) const
+VectorInt VectorInt::operator++(int)
 {
-	if (size != other.size)
-		throw invalid_argument("Vectors must be of the same size");
-	Vector result(size);
-	for (int i = 0; i < size; i++)
-		result.data[i] = data[i] - other.data[i];
-
-	return result;
+	VectorInt temp(*this);
+	++(*this);
+	return temp;
 }
 
-Vector Vector::operator*(short scalar) const
+VectorInt &VectorInt::operator--()
 {
-	Vector result(size);
-	for (int i = 0; i < size; i++)
-		result.data[i] = data[i] * scalar;
-
-	return result;
+	for (int i = 0; i < size; ++i)
+		--data[i];
+	return *this;
 }
 
-bool Vector::operator==(const Vector &other) const
+VectorInt VectorInt::operator--(int)
 {
-	if (size != other.size)
-		return false;
-
-	for (int i = 0; i < size; i++)
-		if (data[i] != other.data[i])
-			return false;
-
-	return true;
+	VectorInt temp(*this);
+	--(*this);
+	return temp;
 }
-
-bool Vector::operator!=(const Vector &other) const { return !(*this == other); }
-bool Vector::operator<(const Vector &other) const { return size < other.size; }
-bool Vector::operator>(const Vector &other) const { return size > other.size; }
-
-int Vector::getState() const { return state; }
-int Vector::getObjectCount() { return objectCount; }
-
-int Vector::objectCount = 0;
